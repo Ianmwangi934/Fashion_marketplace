df20d69dfa4ff84e4f0b871d23f1bd80749b6ae3
ian mwangi <ian_mwangi@localhost.localdomain>
2025-07-02 12:24:17 +0300

    Removed ticket system project for clean deployment

--- backend/Fashion_marketplace/Fashion_marketplace/settings.py
+++ backend/Fashion_marketplace/Fashion_marketplace/settings.py
@@ -27,14 +27,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)s#-*dm-)fa_nn8lzg=z-j0kccb(ron)f+07su8z9snpcfsgma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
