b25df527a3d4f495329d637f68030b17ff3693f7
Ianmwangi934 <ianmurigu4@gmail.com>
2025-07-03 07:47:05 +0000

    Update settings.py

--- backend/Fashion_marketplace/Fashion_marketplace/settings.py
+++ backend/Fashion_marketplace/Fashion_marketplace/settings.py
@@ -136,6 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://fashion-marketplace-nine.vercel.app/"
     
 ]
 REST_FRAMEWORK = {
