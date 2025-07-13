c7257db05ca599620e414bddb8b0237563f23b8d
ian mwangi <ian_mwangi@localhost.localdomain>
2025-07-13 14:09:50 +0300

    Made some changes

--- backend/Fashion_marketplace/Fashion_marketplace/settings.py
+++ backend/Fashion_marketplace/Fashion_marketplace/settings.py
@@ -15,6 +15,7 @@ import os
 from dotenv import load_dotenv
 import stripe
 load_dotenv()
+import cloudinary
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,8 +138,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://fashion-marketplace-nine.vercel.app/",
-    "https://fashion-marketplace-t1y3.vercel.app/products",
     "https://fashion-marketplace-q8gm.vercel.app",
     "https://fashion-marketplace-npm5.vercel.app"
     
@@ -188,6 +187,14 @@ CLOUDINARY_STORAGE = {
     'API_KEY' : 573878397521187,
     'API_SECRET' : '8SbnUx3cYxQN0h_L2cCpAV-lI34',
 }
+
+cloudinary.config( 
+    cloud_name = "dlepagcl0", 
+    api_key = "573878397521187", 
+    api_secret = "8SbnUx3cYxQN0h_L2cCpAV-lI34",
+    secure=True
+)
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
